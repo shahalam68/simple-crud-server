40b4874cf2bc37664ccec132d058cfbe3ad75bcd
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-11-12 21:54:56 +0600

    user

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,7 +18,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://akmshahalam68:beKxEaCaKddq1imA@cluster0.zw4q92x.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,21 +34,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const usersCollections = database.collection("users");
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('new user', user);
+      const result = await usersCollections.insertOne(user);
+      res.send(result)
+    });
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({
+      ping: 1
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.get('/',(req, res)=>{
-    res.send('Simple Card running')
+app.get('/', (req, res) => {
+  res.send('Simple Card running')
 })
-app.listen(port,()=>{
-    console.log(`Simple curd on port: ${port}`)
+app.listen(port, () => {
+  console.log(`Simple curd on port: ${port}`)
 })
