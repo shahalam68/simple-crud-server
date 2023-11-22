69665a91d6db8c9880445d7d7ef2901d8703750d
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-11-22 13:08:04 +0600

    data load

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ async function run() {
     const database = client.db("usersDB");
     const usersCollections = database.collection("users");
 
+    app.get('/users',async(req,res) =>{
+      const cursor = usersCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       console.log('new user', user);
